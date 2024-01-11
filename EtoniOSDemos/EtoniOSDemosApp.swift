ccd1dcc3bb684982b7a896c738d78e5ba10df1de
Matthew Collins <m.collins@etoncollege.org.uk>
2024-01-11 16:11:23 +0000

    Initial Commit

---
+++ EtoniOSDemos/EtoniOSDemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  EtoniOSDemosApp.swift
+//  EtoniOSDemos
+//
+//  Created by Collins, Matthew - MC on 11/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct EtoniOSDemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
